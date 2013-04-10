bd7bc1eb9d48927c5f2b9bd9b8c3a2ea3e0e7138
coddreau <coddreau@gmail.com>
2013-04-10 17:44:53 -0300

    Adding Ace, a rack server and basic html/js

--- wemacs.py
+++ wemacs.py
@@ -3,10 +3,10 @@ import webkit
 
 window = gtk.Window()
 box = gtk.VBox(homogeneous=False, spacing=0)
-browser = webkit.WebView()
+webview = webkit.WebView()
 window.set_default_size(800, 600)
 window.add(box)
-box.pack_start(browser, expand=True, fill=True, padding=0)
+box.pack_start(webview, expand=True, fill=True, padding=0)
 window.show_all()
-browser.open('http://localhost:3000')
+webview.open('http://localhost:5000')
 gtk.main()
