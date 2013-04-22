210c3a9ae321815f736c288026c94e8dd1b609f4
coddreau <coddreau@gmail.com>
2013-04-22 13:18:55 -0300

    Added a chdir to ServerRoot for asset compilation

--- server/lib/assets.rb
+++ server/lib/assets.rb
@@ -2,21 +2,25 @@ require 'coffee-script'
 require 'slim'
 
 module Assets
+  
   def self.compile
-    Dir.glob('javascripts/**/*.coffee').each do |coffee|
-      js = CoffeeScript.compile(File.read(coffee))
-      File.write(coffee.sub(/coffee$/, 'js'), js)
-    end
+    Dir.chdir(ServerRoot) do
+      Dir.glob('javascripts/**/*.coffee').each do |coffee|
+        js = CoffeeScript.compile(File.read(coffee))
+        File.write(coffee.sub(/coffee$/, 'js'), js)
+      end
 
-    Dir.glob('stylesheets/**/*.sass').each do |sass|
-      css = sass.sub(/sass$/, 'css')
-      system "sass #{sass}:#{css}"
-    end
+      Dir.glob('stylesheets/**/*.sass').each do |sass|
+        css = sass.sub(/sass$/, 'css')
+        system "sass #{sass}:#{css}"
+      end
 
-    Dir.glob('views/**/*.slim').each do |slim|
-      html = File.basename(slim).sub(/slim$/, 'html')
-      html = "public/#{html}"
-      File.write(html, Slim::Template.new(slim).render(html))
+      Dir.glob('views/**/*.slim').each do |slim|
+        html = File.basename(slim).sub(/slim$/, 'html')
+        html = "public/#{html}"
+        File.write(html, Slim::Template.new(slim).render(html))
+      end
     end
   end
+  
 end
